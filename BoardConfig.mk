9e6251bbb4b61c547b704aaab6cf9c6d7086aa84
Wes Garner <wesgarner@gmail.com>
2010-07-27 10:23:40 -0700

    Kang from @koush

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,31 @@ BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
 BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
 # The size of a block that can be marked bad.
 BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_FFORMAT := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+
+BOARD_DATA_DEVICE := /dev/block/mmcblk0p2
+BOARD_DATA_FILESYSTEM := rfs
+BOARD_DATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_HAS_DATADATA := true
+BOARD_DATADATA_DEVICE := /dev/block/stl10
+BOARD_DATADATA_FILESYSTEM := rfs
+BOARD_DATADATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SYSTEM_DEVICE := /dev/block/stl9
+BOARD_SYSTEM_FILESYSTEM := rfs
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
+BOARD_CACHE_DEVICE := /dev/block/stl11
+BOARD_CACHE_FILESYSTEM := rfs
+BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+# The following is the *actual* sd card, but those don't mount in recovery.
+# The mmc blocks aren't even available.
+# Only internal storage (mmcblk0) is available.
+#BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1
+#BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 	
 TARGET_RECOVERY_UI_LIB := librecovery_ui_galaxys librecovery_ui_generic
 
