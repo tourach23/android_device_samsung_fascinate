1afa7c1beaa5d5b752b2c8ccc86b930ddd9786ac
Wes Garner <wesgarner@gmail.com>
2010-07-27 13:03:28 -0500

    add libspeech, later to be moved to proprietary

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -154,8 +154,9 @@ PRODUCT_COPY_FILES += \
     device/samsung/galaxys/prebuilt/omx/libswmv7domxoc.so:system/lib/libswmv7domxoc.so \
     device/samsung/galaxys/prebuilt/omx/libswmv8domxoc.so:system/lib/libswmv8domxoc.so \
 
-# libaudio
+# libaudio / libspeech
 PRODUCT_COPY_FILES += device/samsung/galaxys/prebuilt/audio/libaudio.so:system/lib/libaudio.so
+PRODUCT_COPY_FILES += device/samsung/galaxys/prebuilt/audio/libspeech.so:system/lib/libspeech.so
 
 
 # we have enough storage space to hold precise GC data
