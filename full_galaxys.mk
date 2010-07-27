ea05c15ff40efe6f63fd596d3110a604af29520e
Wes Garner <wesgarner@gmail.com>
2010-07-27 13:02:20 -0500

    add libcamera files, later to be moved to proprietary

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -109,6 +109,16 @@ PRODUCT_COPY_FILES += \
     device/samsung/galaxys/prebuilt/vold.fstab:system/etc/vold.fstab 
 
 #
+# Camera
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/galaxys/prebuilt/camera\libarccamera.so:system/lib/libarccamera.so \
+    device/samsung/galaxys/prebuilt/camera\libcamerafirmwarejni.so:system/lib/libcamerafirmwarejni.so \
+    device/samsung/galaxys/prebuilt/camera\libcamera.so:system/lib/libcamera.so \
+    device/samsung/galaxys/prebuilt/camera\libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
+    device/samsung/galaxys/prebuilt/camera\libseccamera.so:system/lib/libseccamera.so
+
+#
 # RIL
 #
 PRODUCT_COPY_FILES += \
