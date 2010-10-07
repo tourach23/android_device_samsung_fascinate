9975afbb7445ac658dd3c27fa81eabbcc2c146d0
Wes Garner <wesgarner@gmail.com>
2010-10-07 13:47:50 -0500

    rename vibrant to SGH-T959
    Change-Id: Iddd3bfa2620fc70ecd348bdf6252baa544ca3819

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,12 +24,13 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/samsung/vibrant/BoardConfigVendor.mk
+-include vendor/samsung/SGH-T959/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 #TARGET_NO_RECOVERY := true
 #TARGET_NO_KERNEL := true
 
+TARGET_BOARD := SGH-T959
 TARGET_BOARD_PLATFORM := s5pc110
 TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
 
@@ -49,7 +50,7 @@ BT_ALT_STACK := true
 BRCM_BTL_INCLUDE_A2DP := true
 BRCM_BT_USE_BTL_IF := true
 
-BOARD_EGL_CFG := device/samsung/vibrant/prebuilt/egl/egl.cfg
+BOARD_EGL_CFG := device/samsung/SGH-T959/prebuilt/egl/egl.cfg
 
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x02e00000
@@ -85,4 +86,4 @@ BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
 BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 #BOARD_USES_BMLUTILS := true
 
-TARGET_PREBUILT_KERNEL := device/samsung/vibrant/kernel
+TARGET_PREBUILT_KERNEL := device/samsung/SGH-T959/kernel
--- full_vibrant.mk
+++ full_vibrant.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/init.smdkc110.rc:root/init.smdkc110.rc
+    device/samsung/SGH-T959/init.smdkc110.rc:root/init.smdkc110.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
@@ -36,17 +36,17 @@ PRODUCT_COPY_FILES += \
 # Wifi
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/wifi/libwlservice.so:system/lib/libwlservice.so \
-    device/samsung/vibrant/prebuilt/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
-    device/samsung/vibrant/prebuilt/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
-    device/samsung/vibrant/prebuilt/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
-    device/samsung/vibrant/prebuilt/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
-    device/samsung/vibrant/prebuilt/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
-    device/samsung/vibrant/prebuilt/wifi/wifi.conf:system/etc/wifi/wifi.conf \
-    device/samsung/vibrant/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/samsung/vibrant/prebuilt/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/samsung/vibrant/prebuilt/wifi/wlservice:system/bin/wlservice \
-    device/samsung/vibrant/prebuilt/wifi/wpa_supplicant:system/bin/wpa_supplicant
+    device/samsung/SGH-T959/prebuilt/wifi/libwlservice.so:system/lib/libwlservice.so \
+    device/samsung/SGH-T959/prebuilt/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    device/samsung/SGH-T959/prebuilt/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    device/samsung/SGH-T959/prebuilt/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
+    device/samsung/SGH-T959/prebuilt/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
+    device/samsung/SGH-T959/prebuilt/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
+    device/samsung/SGH-T959/prebuilt/wifi/wifi.conf:system/etc/wifi/wifi.conf \
+    device/samsung/SGH-T959/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/SGH-T959/prebuilt/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/SGH-T959/prebuilt/wifi/wlservice:system/bin/wlservice \
+    device/samsung/SGH-T959/prebuilt/wifi/wpa_supplicant:system/bin/wpa_supplicant
 
 
 
@@ -54,94 +54,94 @@ PRODUCT_COPY_FILES += \
 # Display (3D)
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/egl/egl.cfg:system/lib/egl/egl.cfg \
-    device/samsung/vibrant/prebuilt/egl/libEGL_POWERVR_SGX540_120.so:system/lib/egl/libEGL_POWERVR_SGX540_120.so \
-    device/samsung/vibrant/prebuilt/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    device/samsung/vibrant/prebuilt/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
-    device/samsung/vibrant/prebuilt/egl/libGLESv2_POWERVR_SGX540_120.so:system/lib/egl/libGLESv2_POWERVR_SGX540_120.so
+    device/samsung/SGH-T959/prebuilt/egl/egl.cfg:system/lib/egl/egl.cfg \
+    device/samsung/SGH-T959/prebuilt/egl/libEGL_POWERVR_SGX540_120.so:system/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    device/samsung/SGH-T959/prebuilt/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    device/samsung/SGH-T959/prebuilt/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    device/samsung/SGH-T959/prebuilt/egl/libGLESv2_POWERVR_SGX540_120.so:system/lib/egl/libGLESv2_POWERVR_SGX540_120.so
 
 #
 # Sensors, Lights etc
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/hw/copybit.s5pc110.so:system/lib/hw/copybit.s5pc110.so \
-    device/samsung/vibrant/prebuilt/hw/lights.default.so:system/lib/hw/lights.default.so \
-    device/samsung/vibrant/prebuilt/hw/sensors.SGH-T959.so:system/lib/hw/sensors.SGH-T959.so \
-    device/samsung/vibrant/prebuilt/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
+    device/samsung/SGH-T959/prebuilt/hw/copybit.s5pc110.so:system/lib/hw/copybit.s5pc110.so \
+    device/samsung/SGH-T959/prebuilt/hw/lights.default.so:system/lib/hw/lights.default.so \
+    device/samsung/SGH-T959/prebuilt/hw/sensors.SGH-T959.so:system/lib/hw/sensors.SGH-T959.so \
+    device/samsung/SGH-T959/prebuilt/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
 
 #
 # Keys
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
-    device/samsung/vibrant/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
-    device/samsung/vibrant/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/samsung/vibrant/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
-    device/samsung/vibrant/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/samsung/vibrant/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
+    device/samsung/SGH-T959/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
+    device/samsung/SGH-T959/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/SGH-T959/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/SGH-T959/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
+    device/samsung/SGH-T959/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/SGH-T959/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
+    device/samsung/SGH-T959/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/SGH-T959/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
+    device/samsung/SGH-T959/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/SGH-T959/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
 
 
 #
 # Vold
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/vold.fstab:system/etc/vold.fstab 
+    device/samsung/SGH-T959/prebuilt/vold.fstab:system/etc/vold.fstab 
 
 #
 # Camera
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/camera/libarccamera.so:system/lib/libarccamera.so \
-    device/samsung/vibrant/prebuilt/camera/libcamerafirmwarejni.so:system/lib/libcamerafirmwarejni.so \
-    device/samsung/vibrant/prebuilt/camera/libcamera.so:system/lib/libcamera.so \
-    device/samsung/vibrant/prebuilt/camera/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
-    device/samsung/vibrant/prebuilt/camera/libseccamera.so:system/lib/libseccamera.so
+    device/samsung/SGH-T959/prebuilt/camera/libarccamera.so:system/lib/libarccamera.so \
+    device/samsung/SGH-T959/prebuilt/camera/libcamerafirmwarejni.so:system/lib/libcamerafirmwarejni.so \
+    device/samsung/SGH-T959/prebuilt/camera/libcamera.so:system/lib/libcamera.so \
+    device/samsung/SGH-T959/prebuilt/camera/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
+    device/samsung/SGH-T959/prebuilt/camera/libseccamera.so:system/lib/libseccamera.so
 
 #
 # RIL
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/ril/libril.so:system/lib/libril.so \
-    device/samsung/vibrant/prebuilt/ril/libreference-ril.so:system/lib/libreference-ril.so \
-    device/samsung/vibrant/prebuilt/ril/libsecril-client.so:system/lib/libsecril-client.so \
-    device/samsung/vibrant/prebuilt/ril/drexe:system/bin/drexe \
-    device/samsung/vibrant/prebuilt/ril/efsd:system/bin/efsd \
-    device/samsung/vibrant/prebuilt/ril/efsd30:system/bin/efsd30 \
-    device/samsung/vibrant/prebuilt/ril/rilclient-test:system/bin/rilclient-test
+    device/samsung/SGH-T959/prebuilt/ril/libril.so:system/lib/libril.so \
+    device/samsung/SGH-T959/prebuilt/ril/libreference-ril.so:system/lib/libreference-ril.so \
+    device/samsung/SGH-T959/prebuilt/ril/libsecril-client.so:system/lib/libsecril-client.so \
+    device/samsung/SGH-T959/prebuilt/ril/drexe:system/bin/drexe \
+    device/samsung/SGH-T959/prebuilt/ril/efsd:system/bin/efsd \
+    device/samsung/SGH-T959/prebuilt/ril/efsd30:system/bin/efsd30 \
+    device/samsung/SGH-T959/prebuilt/ril/rilclient-test:system/bin/rilclient-test
 
 #
 # OMX
 #
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/omx/libs263domxoc.so:system/lib/libs263domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libs263eomxoc.so:system/lib/libs263eomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libs264domxoc.so:system/lib/libs264domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libs264eomxoc.so:system/lib/libs264eomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsaacdomxoc.so:system/lib/libsaacdomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsaaceomxoc.so:system/lib/libsaaceomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsac3domxoc.so:system/lib/libsac3domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsamrdomxoc.so:system/lib/libsamrdomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsamreomxoc.so:system/lib/libsamreomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsdiv3domxoc.so:system/lib/libsdiv3domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsflacdomxoc.so:system/lib/libsflacdomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsmp3domxoc.so:system/lib/libsmp3domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsmp4fmocn.so:system/lib/libsmp4fmocn.so \
-    device/samsung/vibrant/prebuilt/omx/libsmp4vdomxoc.so:system/lib/libsmp4vdomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsmp4veomxoc.so:system/lib/libsmp4veomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libsvc1domxoc.so:system/lib/libsvc1domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libswmadomxoc.so:system/lib/libswmadomxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libswmv7domxoc.so:system/lib/libswmv7domxoc.so \
-    device/samsung/vibrant/prebuilt/omx/libswmv8domxoc.so:system/lib/libswmv8domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libs263domxoc.so:system/lib/libs263domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libs263eomxoc.so:system/lib/libs263eomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libs264domxoc.so:system/lib/libs264domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libs264eomxoc.so:system/lib/libs264eomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsaacdomxoc.so:system/lib/libsaacdomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsaaceomxoc.so:system/lib/libsaaceomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsac3domxoc.so:system/lib/libsac3domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsamrdomxoc.so:system/lib/libsamrdomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsamreomxoc.so:system/lib/libsamreomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsdiv3domxoc.so:system/lib/libsdiv3domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsflacdomxoc.so:system/lib/libsflacdomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsmp3domxoc.so:system/lib/libsmp3domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsmp4fmocn.so:system/lib/libsmp4fmocn.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsmp4vdomxoc.so:system/lib/libsmp4vdomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsmp4veomxoc.so:system/lib/libsmp4veomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libsvc1domxoc.so:system/lib/libsvc1domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libswmadomxoc.so:system/lib/libswmadomxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libswmv7domxoc.so:system/lib/libswmv7domxoc.so \
+    device/samsung/SGH-T959/prebuilt/omx/libswmv8domxoc.so:system/lib/libswmv8domxoc.so \
 
 # libspeech
-PRODUCT_COPY_FILES += device/samsung/vibrant/prebuilt/audio/libspeech.so:system/lib/libspeech.so
+PRODUCT_COPY_FILES += device/samsung/SGH-T959/prebuilt/audio/libspeech.so:system/lib/libspeech.so
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/vibrant/kernel
+LOCAL_KERNEL := device/samsung/SGH-T959/kernel
 else
 LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -162,6 +162,6 @@ $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_NAME := full_vibrant
 PRODUCT_DEVICE := SGH-T959
-PRODUCT_MODEL := SGH-T959
+PRODUCT_MODEL := Vibrant
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
